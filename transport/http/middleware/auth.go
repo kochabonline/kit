9cc4c6f2ebdbd74886c463a7329952c45099c2a5
kochabonline <kochabonline@gmail.com>
2024-06-26 09:58:30 +0800

    fix auth middleware

--- transport/http/middleware/auth.go
+++ transport/http/middleware/auth.go
@@ -1,57 +1,49 @@
 package middleware
 
 import (
-	"strconv"
-
 	"github.com/gin-gonic/gin"
 	"github.com/kochabonline/kit/errors"
 	"github.com/kochabonline/kit/log"
 	"github.com/kochabonline/kit/transport/http/response"
 )
 
-var ErrUnauthorized = errors.Unauthorized("unauthorized", "unauthorized")
+var ErrUnauthorized = errors.Unauthorized("unauthorized", "auth middleware failed to authorize request")
 
 type AuthConfig struct {
 	AuthHeader string
-	ParseId    func(c *gin.Context) (int64, error)
+	ParseAuth  func(c *gin.Context) (string, error)
 }
 
 func AuthWithConfig(config AuthConfig) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		headerUserId, err := parseHeaderId(c, config.AuthHeader)
-		if err != nil {
-			log.Error("failed to parse header id", "error", err)
-			response.GinJSONError(c, err)
+		if config.ParseAuth == nil {
+			c.Next()
 			return
 		}
 
-		userId, err := config.ParseId(c)
-		if err != nil {
-			log.Error("failed to parse id", "error", err)
-			response.GinJSONError(c, err)
-			return
-		}
-
-		if headerUserId != userId {
-			log.Error("unauthorized", "headerUserId", headerUserId, "userId", userId)
+		authHeader := c.GetHeader(config.AuthHeader)
+		if authHeader == "" {
+			log.Errorf("missing auth header %s", config.AuthHeader)
 			response.GinJSONError(c, ErrUnauthorized)
+			c.Abort()
+			return
+		}
+
+		auth, err := config.ParseAuth(c)
+		if err != nil {
+			log.Error("failed to parse auth", "error", err)
+			response.GinJSONError(c, ErrUnauthorized)
+			c.Abort()
+			return
+		}
+
+		if authHeader != auth {
+			log.Error("unauthorized", "authHeader", authHeader, "auth", auth)
+			response.GinJSONError(c, ErrUnauthorized)
+			c.Abort()
 			return
 		}
 
 		c.Next()
 	}
-}
-
-func parseHeaderId(c *gin.Context, header string) (int64, error) {
-	id := c.GetHeader(header)
-	if id == "" {
-		return 0, errors.BadRequest("missing header", "missing header %s", header)
-	}
-
-	parse, err := strconv.ParseInt(id, 10, 64)
-	if err != nil {
-		return 0, errors.BadRequest("invalid header", "invalid header %s", id)
-	}
-
-	return parse, nil
 }
