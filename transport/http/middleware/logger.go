3126b1a1013cf290681f56d91502b377576048ae
kochab <kochabonline@gmail.com>
2024-09-08 17:06:49 +0800

    fix logger middleware

--- transport/http/middleware/logger.go
+++ transport/http/middleware/logger.go
@@ -23,14 +23,23 @@ func GinLoggerWithConfig(config GinLoggerConfig) gin.HandlerFunc {
 		c.Next()
 		cost := time.Since(start)
 
-		params := make([]any, 0, 10)
+		params := make([]any, 0, 12)
 		params = append(params,
+			"timestamp", time.Now().Format(time.RFC3339),
 			"method", c.Request.Method,
 			"uri", c.Request.RequestURI,
 			"cost", cost.String(),
 			"status", c.Writer.Status(),
 			"client_ip", c.ClientIP(),
+			"query", c.Request.URL.Query(),
+			"header", c.Request.Header,
+			"body", c.Request.Body,
+			"user_agent", c.Request.UserAgent(),
 		)
+
+		if requestId := c.Writer.Header().Get("X-Request-ID"); requestId != "" {
+			params = append(params, "request_id", requestId)
+		}
 
 		if len(c.Errors) > 0 {
 			params = append(params, "errors", c.Errors.ByType(gin.ErrorTypePrivate).String())
