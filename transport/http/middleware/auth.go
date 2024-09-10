aae6fc624e1e32be51187b510da3cd3fd62be29a
kochab <kochabonline@gmail.com>
2024-09-10 14:51:06 +0800

    fix auth middleware

--- transport/http/middleware/auth.go
+++ transport/http/middleware/auth.go
@@ -1,10 +1,18 @@
 package middleware
 
 import (
+	"context"
+
 	"github.com/gin-gonic/gin"
 	"github.com/kochabonline/kit/errors"
 	"github.com/kochabonline/kit/log"
 	"github.com/kochabonline/kit/transport/http/response"
+)
+
+type contextKey string
+
+const (
+	Token = contextKey("token")
 )
 
 var ErrUnauthorized = errors.Unauthorized("unauthorized", "auth middleware failed to authorize request")
@@ -13,7 +21,7 @@ type AuthConfig struct {
 	// AuthHeader is the header key to look for the auth value
 	AuthHeader string
 	// Validate is a function that takes a gin context and returns the auth value
-	Validate func(c *gin.Context) (string, error)
+	Validate func(c *gin.Context) (any, string, error)
 	// SkippedPathPrefixes is a list of path prefixes that should be skipped from auth
 	SkippedPathPrefixes []string
 }
@@ -36,7 +44,7 @@ func AuthWithConfig(config AuthConfig) gin.HandlerFunc {
 			return
 		}
 
-		auth, err := config.Validate(c)
+		token, auth, err := config.Validate(c)
 		if err != nil {
 			log.Error("unauthorized", "authHeader", authHeader, "request", c.Request, "error", err)
 			response.GinJSONError(c, ErrUnauthorized)
@@ -48,6 +56,10 @@ func AuthWithConfig(config AuthConfig) gin.HandlerFunc {
 			response.GinJSONError(c, ErrUnauthorized)
 			return
 		}
+
+		// Set the token in the context
+		ctx := context.WithValue(c.Request.Context(), Token, token)
+		c.Request = c.Request.WithContext(ctx)
 
 		c.Next()
 	}
