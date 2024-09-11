e59abae184d683fff355da3260b6b026a9847018
kochab <kochabonline@gmail.com>
2024-09-11 16:45:42 +0800

    add casbin middleware

--- transport/http/middleware/auth.go
+++ transport/http/middleware/auth.go
@@ -11,18 +11,21 @@ import (
 )
 
 const (
-	Token = "token"
+	defaultToken = "token"
 )
 
 var (
-	ErrUnauthorized   = errors.Unauthorized("unauthorized", "auth middleware failed to authorize request")
-	ErrHeaderNotFound = errors.BadRequest("missing header", "auth middleware failed to authorize request")
-	ErrNotLoggedIn    = errors.BadRequest("not logged in", "auth middleware failed to authorize request")
+	ErrAuthHeaderNotFound = errors.BadRequest("missing header", "auth middleware failed to authorize request")
+	ErrAuthNotLoggedIn    = errors.BadRequest("not logged in", "auth middleware failed to authorize request")
+	ErrAuthUnauthorized   = errors.Unauthorized("unauthorized", "auth middleware failed to authorize request")
+	ErrAuthForbidden      = errors.Forbidden("permission denied", "auth middleware failed to authorize request")
 )
 
 type AuthConfig struct {
 	// AuthHeader is the header key to look for the auth value
 	AuthHeader string
+	// TokenName is the header key to look for the auth value
+	Token string
 	// Validate is a function that takes a gin context and returns the auth value
 	// The contents in the map will be put into the context
 	// The fields that must be included are: userId, header, token
@@ -33,19 +36,18 @@ type AuthConfig struct {
 
 func AuthWithConfig(config AuthConfig) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if config.Validate == nil {
+		if config.Validate == nil || skippedPathPrefixes(c, config.SkippedPathPrefixes...) {
 			c.Next()
 			return
 		}
-		if skippedPathPrefixes(c, config.SkippedPathPrefixes...) {
-			c.Next()
-			return
+		if config.Token == "" {
+			config.Token = defaultToken
 		}
 
 		authHeader := c.GetHeader(config.AuthHeader)
 		if authHeader == "" {
 			log.Errorf("unauthorized: missing auth header: %s", config.AuthHeader)
-			response.GinJSONError(c, ErrHeaderNotFound)
+			response.GinJSONError(c, ErrAuthHeaderNotFound)
 			return
 		}
 
@@ -53,22 +55,22 @@ func AuthWithConfig(config AuthConfig) gin.HandlerFunc {
 		if err != nil {
 			if errors.Is(err, gorm.ErrRecordNotFound) {
 				log.Errorf("unauthorized: %s not logged in: %v", authHeader, err)
-				response.GinJSONError(c, ErrNotLoggedIn)
+				response.GinJSONError(c, ErrAuthNotLoggedIn)
 				return
 			}
 			log.Errorf("unauthorized: %s failed to validate auth: %v", authHeader, err)
-			response.GinJSONError(c, ErrUnauthorized)
+			response.GinJSONError(c, ErrAuthUnauthorized)
 			return
 		}
-		header, token := result[config.AuthHeader], result[Token]
+		header, token := result[config.AuthHeader], result[config.Token]
 		if authHeader != header {
 			log.Errorf("unauthorized: not match header, expected: %s, got: %s", header, authHeader)
-			response.GinJSONError(c, ErrUnauthorized)
+			response.GinJSONError(c, ErrAuthUnauthorized)
 			return
 		}
 		if token == nil {
 			log.Errorf("unauthorized: %s not logged in: token is nil", authHeader)
-			response.GinJSONError(c, ErrNotLoggedIn)
+			response.GinJSONError(c, ErrAuthNotLoggedIn)
 			return
 		}
 
---
+++ transport/http/middleware/permission.go
@@ -0,0 +1,55 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/kochabonline/kit/errors"
+	"github.com/kochabonline/kit/log"
+	"github.com/kochabonline/kit/transport/http/response"
+)
+
+var (
+	ErrPermissionUnauthorized = errors.Forbidden("permission denied", "permission middleware failed to authorize request")
+)
+
+type PermissionConfig struct {
+	// Param is the param key to look for the id value
+	Param string
+	// Role is the key to look for the role value
+	Role string
+	// Validate is a function that takes a gin context and returns the auth value
+	Validate func(c *gin.Context) (id string, role string, err error)
+	// SkippedRoles is a list of roles that should be skipped from permission
+	SkippedRoles []string
+}
+
+func PermissionWithConfig(config PermissionConfig) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if config.Validate == nil || config.Param == "" || config.Role == "" {
+			c.Next()
+			return
+		}
+
+		id, role, err := config.Validate(c)
+		if err != nil {
+			log.Errorf("permission failed to validate: %v", err)
+			response.GinJSONError(c, ErrPermissionUnauthorized)
+			return
+		}
+
+		for _, skippedRole := range config.SkippedRoles {
+			if role == skippedRole {
+				c.Next()
+				return
+			}
+		}
+
+		paramValue := c.Param(config.Param)
+		if paramValue != "" && paramValue != id {
+			log.Errorf("permission denied access to %s for %s", paramValue, id)
+			response.GinJSONError(c, ErrPermissionUnauthorized)
+			return
+		}
+
+		c.Next()
+	}
+}
